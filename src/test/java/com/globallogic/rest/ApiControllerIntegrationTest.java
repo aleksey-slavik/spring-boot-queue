cbb5d55d2a0312515f25d27c73b7fd6e9c1327f6
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-04-02 17:14:14 +0300

    add tests

---
+++ src/test/java/com/globallogic/rest/ApiControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.globallogic.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private URL url;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void init() throws Exception {
+        this.url = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void indexTest() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(url.toString(), String.class);
+        assertEquals("Hello world!", response.getBody());
+    }
+}
