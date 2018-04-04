050c1fa9246e618f084451cba963b72cf77561ed
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-04-04 15:01:25 +0300

    add modules for ElasticMQ and MessageService

---
+++ spring-message-service/src/main/java/com/globallogic/SpringMessageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.globallogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessageServiceApplication.class, args);
+	}
+}
