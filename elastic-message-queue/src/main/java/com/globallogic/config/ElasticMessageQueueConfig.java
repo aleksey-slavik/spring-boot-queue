f796f3f6441cceb828dfef55d9f3080cbbcbc667
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-04-04 17:54:22 +0300

    update elasticMQ config

--- elastic-message-queue/src/main/java/com/globallogic/config/ElasticMessageQueueConfig.java
+++ elastic-message-queue/src/main/java/com/globallogic/config/ElasticMessageQueueConfig.java
@@ -1,39 +1,73 @@
 package com.globallogic.config;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.sqs.AmazonSQSClient;
-import org.springframework.beans.factory.annotation.Value;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
+import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
+import com.amazonaws.services.sqs.buffered.QueueBufferConfig;
+import org.elasticmq.rest.sqs.SQSRestServer;
+import org.elasticmq.rest.sqs.SQSRestServerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class ElasticMessageQueueConfig {
 
-    @Value("${queue.accessKey}")
-    private String accessKey;
+    private static final String ACCESS_KEY = "";
 
-    @Value("${queue.secretKey}")
-    private String secretKey;
+    private static final String SECRET_KEY = "";
 
-    @Value("${queue.endpoint}")
-    private String endpoint;
+    private static final String QUEUE_SCHEME = "http";
 
-    @Value("${queue.queueName}")
-    private String queueName;
+    private static final String QUEUE_HOST = "localhost";
+
+    private static final String QUEUE_NAME = "spring-boot-queue";
+
+    private static final int QUEUE_PORT = 9324;
+
+    private static final long QUEUE_MAX_BATCH_OPEN = 200;
+
+    private static final int QUEUE_MAX_BATCH_SIZE = 10;
+
+    private static final int QUEUE_MAX_INFLIGHT_OUTBOUND_BATCHES = 5;
 
     @Bean
-    public AmazonSQSClient amazonSQSClient() {
-        AmazonSQSClient client = new AmazonSQSClient(new BasicAWSCredentials(accessKey, secretKey));
-        client.setEndpoint(endpoint);
-        client.createQueue(queueName);
-        return client;
+    public UriComponents elasticMQLocalSQSUri() {
+        return UriComponentsBuilder.newInstance()
+                .scheme(QUEUE_SCHEME)
+                .host(QUEUE_HOST)
+                .port(QUEUE_PORT)
+                .build()
+                .encode();
     }
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+    public SQSRestServer sqsRestServer(UriComponents uri) {
+        return SQSRestServerBuilder
+                .withPort(uri.getPort())
+                .withInterface(uri.getHost())
+                .start();
+    }
+
+    @Bean
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+    }
+
+    @Bean
+    public AmazonSQSAsync amazonSQS(AWSCredentials credentials, UriComponents uri) {
+        AmazonSQSAsyncClient client = new AmazonSQSAsyncClient(credentials);
+        client.setEndpoint(uri.toUriString());
+        client.createQueue(QUEUE_NAME);
+
+        QueueBufferConfig config = new QueueBufferConfig()
+                .withMaxBatchOpenMs(QUEUE_MAX_BATCH_OPEN)
+                .withMaxBatchSize(QUEUE_MAX_BATCH_SIZE)
+                .withMaxInflightOutboundBatches(QUEUE_MAX_INFLIGHT_OUTBOUND_BATCHES);
+
+        return new AmazonSQSBufferedAsyncClient(client, config);
     }
 }
